875f445eef9c1a961ebf6404ece59b4ba93af5d7
Damir <damir.ptn@gmail.com>
2024-01-23 15:57:18 +0600

    create models Jobs and UserProfile

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Jobs(models.Model):
+    images = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    price = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    data = models.DateTimeField(max_length=255)
+
+    class Meta:
+        verbose_name = "Подработки"
+        verbose_name_plural = "Подработки"
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,2 +1,15 @@
+# models.py
 from django.db import models
+from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, verbose_name="Имя")
+    last_name = models.CharField(max_length=255, verbose_name="Фамилия")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    photo = models.ImageField(upload_to='profile_photos/', verbose_name="Фото")
+    registration_date = models.DateField(auto_now_add=True, verbose_name="Дата регистрации")
+
+    class Meta:
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
