2353ea0cc3d3f55f5739b8e4194c8d994e463b22
damir <damir.ptn@gmail.com>
2024-01-25 09:14:59 +0600

    upgrade models

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 class Jobs(models.Model):
-    images = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    images = models.ManyToManyField('Image', blank=True)
     price = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
-    data = models.DateTimeField(max_length=255)
+    STATUS_CHOICES = [
+        ('active', 'Актуально'),
+        ('inactive', 'Не актуально'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
+    start_datatime = models.DateTimeField()
+    end_date = models.DateField()
 
     class Meta:
         verbose_name = "Подработки"
         verbose_name_plural = "Подработки"
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='job_images/')
+
+    def __str_(self):
+        return str(self.image)
--- apps/users/models.py
+++ apps/users/models.py
@@ -3,11 +3,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'Мужчина'),
+        ('female', 'Женщина'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255, verbose_name="Имя")
     last_name = models.CharField(max_length=255, verbose_name="Фамилия")
     phone = models.CharField(max_length=20, verbose_name="Телефон")
     photo = models.ImageField(upload_to='profile_photos/', verbose_name="Фото")
+    email = models.EmailField(verbose_name="Электронная почта")
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, verbose_name="Пол")
+    age = models.PositiveIntegerField(verbose_name="Возраст")
+    rating = models.FloatField(default=0.0, verbose_name="Рейтинг")
     registration_date = models.DateField(auto_now_add=True, verbose_name="Дата регистрации")
 
     class Meta:
